1a528ed1050ffc703916337e23300703f31310e8
Vasyokkrut <vasiliybytenko@gmail.com>
2021-01-11 11:52:52 +0300

    mongoose settings moved from main file to env file

--- env.js
+++ env.js
@@ -1,3 +1,10 @@
 // this is very secret key for JWT auth and nobody should know it
-exports.JWTSecretKey = 'eL49I2T2kcm4abXs2uj8e1b7fGHli8e7fnzfb43Do36h5bY9cB96d7517PQ23f0vW'
+// the JWTSecretKey password key string is just an example
+// you should change it in production to a large random string
+exports.JWTSecretKey = 'JWTSecretKey'
+
+// MongoDB url
 exports.mongoURL = 'mongodb://localhost:27017/VasyokkrutProjectDB'
+
+// this is mongoose config object
+exports.mongoSettings = {useNewUrlParser: true, useUnifiedTopology: true}
