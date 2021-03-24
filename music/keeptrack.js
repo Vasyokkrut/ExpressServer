7e70427c874ec5a41bd6f5d1b18224f42c86262b
Vasyokkrut <vasiliybytenko@gmail.com>
2021-03-24 10:17:50 +0300

    just a file to keep track music folder

---
+++ music/keeptrack.js
@@ -0,0 +1,4 @@
+// since we won't push all of the users audio tracks in git,
+// but we have to have the music folder after cloning this repo,
+// this file just added for keep tracking music folder by git
+console.log('this file is just for keep music folder tracking by git')
